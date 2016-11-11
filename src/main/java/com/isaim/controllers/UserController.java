2e2176b235e46e417eaa793ed48cf2ca75599486
Isai Michel Lombera <isaimichellombera@gmail.com>
2016-11-10 23:31:11 -0800

    add docker file and db update for docker

--- src/main/java/com/isaim/controllers/UserController.java
+++ src/main/java/com/isaim/controllers/UserController.java
@@ -4,11 +4,14 @@ import com.isaim.models.User;
 import com.isaim.models.UserDao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@CrossOrigin
+@RestController
 public class UserController {
 
 	@Autowired
@@ -16,25 +19,29 @@ public class UserController {
 
 	// ALTA
 	@RequestMapping("/alta")
-	@ResponseBody
-	public String alta(String nombre, String apellido_materno, String apellido_paterno) {
+	//@ResponseBody
+	//public String alta(String nombre, String apellido_materno, String apellido_paterno) {
+	public User alta(String nombre, String apellido_materno, String apellido_paterno) {
 		String userId = "";
+		User user = null;
 
 		try {
-			User user = new User(nombre, apellido_materno, apellido_paterno);
+			user = new User(nombre, apellido_materno, apellido_paterno);
 			userDao.save(user);
 			userId = String.valueOf(user.getId());
 		}
 		catch (Exception e) {
-			return "Eror en usuario ALTA: " + e.toString();
+			//return "Eror en usuario ALTA: " + e.toString();
+			return user;
 		}
 
-		return "Exito en usuario ALTA con id: " + userId;
+		//return "Exito en usuario ALTA con id: " + userId;
+		return user;
 	}
 
 	// BAJA
 	@RequestMapping("/baja")
-	@ResponseBody
+	//@ResponseBody
 	public String baja(long id) {
 		try {
 			User user = new User(id);
@@ -47,37 +54,45 @@ public class UserController {
 		return "Exito en usuario BAJA con id: " + id;
 	}
 
-	//
+	// CONSULTA
 	@RequestMapping("/por-nombre")
-	@ResponseBody
-	public String porNombre(String nombre) {
+	//@ResponseBody
+	//public String porNombre(String nombre) {
+	public User porNombre(String nombre) {
 		String userId = "";
+		User user = null;
 		try {
-			User user = userDao.findByNombre(nombre);
+			user = userDao.findByNombre(nombre);
 			userId = String.valueOf(user.getId());
 		}
 		catch (Exception e) {
-			return "Eror usuario no encontrado";
+			//return "Eror usuario no encontrado";
+			return user;
 		}
 
-		return "Exito, id de usuario es: " + userId;
+		//return "Exito, id de usuario es: " + userId;
+		return user;
 	}
 
 	// CAMBIOS
 	@RequestMapping("/cambios")
-	@ResponseBody
-	public String cambiosUsuario(long id, String nombre, String apellido_materno, String apellido_paterno) {
+	//@ResponseBody
+	//public String cambiosUsuario(long id, String nombre, String apellido_materno, String apellido_paterno) {
+	public User cambiosUsuario(long id, String nombre, String apellido_materno, String apellido_paterno) {
+		User user = null;
 		try {
-			User user = userDao.findOne(id);
+			user = userDao.findOne(id);
 			user.setNombre(nombre);
 			user.setApellidoMaterno(apellido_materno);
 			user.setApellidoPaterno(apellido_paterno);
 			userDao.save(user);
 		}
 		catch (Exception e) {
-			return "Eror en usuario CAMBIOS con: " + e.toString();
+			//return "Eror en usuario CAMBIOS con: " + e.toString();
+			return user;
 		}
 
-		return "Exito en usuario CAMBIOS con id: " + id;
+		//return "Exito en usuario CAMBIOS con id: " + id;
+		return user;
 	}
 }
